231acd85b718a86fab71e5f92bf05ed37f922152
Jason Sonnichsen <Harliquinn@gmail.com>
2020-03-30 14:36:39 -0400

    Home page added with generator

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from 'react-dom'
-import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 
+import ReactDOM from 'react-dom'
+import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import Routes from 'src/Routes'
 
 import './index.css'
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>New Pretty Text</p>
+    </div>
+  )
+}
+
+export default HomePage
